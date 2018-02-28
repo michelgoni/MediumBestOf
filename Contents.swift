8eaa4d4f4d57e82711105a57ff75441a5078b304
Michel Goñi <michelgoni2@gmail.com>
2018-02-28 17:21:25 +0100

    Added a couple of examples using autoclosures

--- Contents.swift
+++ Contents.swift
@@ -1,21 +1,48 @@
 import UIKit
 /*:
- ## Nice uses case for Autoclusoures
+ ## Nice uses case for Autoclusoures [from Swift by Sundell](https://www.swiftbysundell.com/posts/using-autoclosure-when-designing-swift-apis?utm_campaign=This%2BWeek%2Bin%2BSwift&utm_medium=email&utm_source=This_Week_in_Swift_133)
 
  - Using autoclosure in order to defer an execution
  
- 
  */
-func assert(_ expression: @autoclosure () -> Bool,
-            _ message: @autoclosure () -> String) {
+func assert(_ expression: @autoclosure () -> Bool, _ message: @autoclosure () -> String) {
     
-    // Inside assert we can refer to expression as a normal closure
     if !expression() {
         assertionFailure(message())
     }
 }
 func gimmeSomeBoolean() -> Bool {
-    return false
+    return true
 }
 
 assert(gimmeSomeBoolean(), "Seems something was wrong")
+
+/*:
+ - Avoid {} syntax
+*/
+
+func animate(_ animation: @autoclosure @escaping () -> Void, duration: TimeInterval = 0.25) {
+    UIView.animate(withDuration: duration, animations: animation)
+}
+animate(UIView().frame.origin.y = 100)
+
+/*:
+ - Type inference using default values
+ */
+
+let dictionary : [String: Any?] = ["someValue" : nil ]
+
+extension Dictionary where Value == Any? {
+    
+    func value<T>(forKey key: Key, defaultValue: @autoclosure () -> T) -> T {
+        guard let value = self[key] as? T else {
+            return defaultValue()
+        }
+        
+        return value
+    }
+}
+
+let value = dictionary.value(forKey: "someValue", defaultValue: 100)
+
+
--- README.md
+++
@@ -1,7 +0,0 @@
-# MediumBestOf
-Hands on with some nice Medium articles dealing with aspects, curiosities and hidden functionalities about swift:
-
-
-
-* **Autoclosures**
-
