e5a69016cd839f78952a2a6c4464040515d5cae6
Michel Goñi <michelgoni2@gmail.com>
2018-05-05 16:28:29 +0200

    Added new example

--- Pages/PoPvsSubclass.xcplaygroundpage/Contents.swift
+++ Pages/PoPvsSubclass.xcplaygroundpage/Contents.swift
@@ -12,6 +12,7 @@ import UIKit
 
 protocol Shareable {
     func share(url: String)
+    func showAlert(message: String)
 }
 
 extension Shareable where Self: UIViewController {
@@ -20,18 +21,27 @@ extension Shareable where Self: UIViewController {
             print(url)
         }
     }
+    func showAlert(message: String) {
+        let alert = UIAlertController(title: "error".localized, message: message, preferredStyle: .alert)
+        alert.addAction(UIAlertAction(title: "dialogconfirm", style: .default, handler: nil))
+        present(alert, animated: true, completion: nil)
+    }
 }
 
 class ViewController: UIViewController, Shareable {
     func shareButtonPressed() {
         let url = "http://yayoc.com"
         share(url: url)
-        
+    }
+    
+    func showAlertFmMainVC(){
+        showAlert(message: "This is a cutom mesage")
     }
 }
 
 let myVC = ViewController()
 myVC.shareButtonPressed()
+myVC.showAlertFmMainVC()
 
 
 //: [Next](@next)
